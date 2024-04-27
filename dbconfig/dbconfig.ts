6d05cb5751d7228a177b1f6e2bc9b6d24f3ea9e4
Sarwar Shaikh <55225509+sarwarali847@users.noreply.github.com>
2024-04-27 19:12:02 +0530

    Update dbconfig.ts
    Fixed type mistake in connection statement.

--- dbconfig/dbconfig.ts
+++ dbconfig/dbconfig.ts
@@ -4,11 +4,11 @@ import mongoose from "mongoose";
 export async function connect() {
     try {
         mongoose.connect(process.env.MONGO_URI!);
-        const conenction = mongoose.conenction;
-        conenction.on('connected', () => {
+        const connection = mongoose.connection;
+        connection.on('connected', () => {
             console.log('MongoDB Connected Successfully!')
         })
-        conenction.on('error',(err: string) => {
+        connection.on('error',(err: string) => {
             console.log("MongoDB Connection Error "+err);
             process.exit();
         })
